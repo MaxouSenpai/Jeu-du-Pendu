b28927be623107b0bde602e6ce3501cb8d675742
Maxime <hauwaertmaxime@gmail.com>
2017-10-24 20:40:32 +0200

    Function sorted + minor corrections made

--- Desktop/Projets/projet2.py
+++ Desktop/Projets/projet2.py
@@ -2,35 +2,10 @@
 import turtle as t
 import random as r
 
-def iscorrect(l):
-	return l.isalpha() and len(l) == 1
-	
-def posbegin(w):
-	if len(w) % 2 == 0:
-		res = ((-(len(w) / 2 - 1) * 50)-5,-150)
-	else:
-		res = (-(len(w)//2)*50,-150)
-	return res
-
-def shadow_word(w):
-	if len(w) % 2 == 0:
-		p_s.setpos(-5,-150)
-		p_s.backward((len(w) / 2 - 1) * 50)
-		for _ in range(len(w)):
-			p_s.write("_",False,align = "center",font = ("Arial",30,"normal"))
-			p_s.forward(50)
-	else:
-		p_s.setpos(0,-150)
-		p_s.backward(len(w)//2*50)
-		for _ in range(len(w)):
-			p_s.write("_",False,align = "center",font = ("Arial",30,"normal"))
-			p_s.forward(50)
-
-def reveal(l,i):
-	p_l.forward(i*50)
-	p_l.write(letter,False,align = "center",font = ("Arial",30,"normal"))
-
 def error(e):
+	"""
+	Fonction prenant en entrée le nombre d'erreurs et permettant de continuer le dessin du bonhomme pendu
+	"""
 	if e == 1:
 		p_e.speed(0)
 		p_e.forward(400)
@@ -49,29 +24,69 @@ def error(e):
 		p_e.penup()
 		p_e.forward(40)
 		p_e.pendown()
-	elif e ==3:
+	elif e == 3:
 		p_e.forward(100)
 		p_e.backward(80)
-	elif e ==4:
+	elif e == 4:
 		p_e.left(30)
 		p_e.forward(50)
 		p_e.backward(50)
 		p_e.right(30)
-	elif e ==5:
+	elif e == 5:
 		p_e.right(30)
 		p_e.forward(50)
 		p_e.backward(50)
 		p_e.left(30)
-	elif e ==6:
+	elif e == 6:
 		p_e.forward(80)
 		p_e.left(30)
 		p_e.forward(50)
 		p_e.backward(50)
 		p_e.right(30)
-	elif e ==7:
+	elif e == 7:
 		p_e.right(30)
 		p_e.forward(50)
 		t.bgcolor("red")
+
+def shadow_word(w): #Toutes les opérations présentes dans cette fonction permettent de tout centrer
+	"""
+	Fonction prenant en entrée un mot et qui permet d'afficher les "_" pour chaque lettre du mot
+	"""
+	if len(w) % 2 == 0:
+		p_s.setpos(-5,-150)
+		p_s.backward((len(w) / 2 - 1) * 50)
+		for _ in range(len(w)):
+			p_s.write("_",False,align = "center",font = ("Arial",30,"normal"))
+			p_s.forward(50)
+	else:
+		p_s.setpos(0,-150)
+		p_s.backward(len(w)//2*50)
+		for _ in range(len(w)):
+			p_s.write("_",False,align = "center",font = ("Arial",30,"normal"))
+			p_s.forward(50)
+
+def originpos(w):
+	"""
+	Fonction prenant en entrée un mot et qui renvoie la position sur Turtle où le mot doit débuter pour qu'il soit centré
+	"""
+	if len(w) % 2 == 0:
+		pos = ((-(len(w) / 2 - 1) * 50)-5,-150)
+	else:
+		pos = (-(len(w)//2)*50,-150)
+	return pos
+
+def reveal(l,i):
+	"""
+	Fonction prenant en entrée une lettre et son indice et qui permet d'afficher la lettre à sa place
+	"""
+	p_l.forward(i*50)
+	p_l.write(letter,False,align = "center",font = ("Arial",30,"normal"))
+
+def iscorrect(l):
+	"""
+	Fonction renvoyant True seulement si l'entrée est une seule lettre
+	"""
+	return l.isalpha() and len(l) == 1
 
 t.title("Jeu du Pendu")
 
@@ -79,10 +94,10 @@ words = [x.strip().lower() for x in open("mots.txt")]
 
 replay = 1
 
-p_e = t.Pen() #Pen Error
-p_s = t.Pen() #Pen Shadow Word
-p_l = t.Pen() #Pen Letter
-p_t_l = t.Pen() #Pen Tested Letter
+p_e = t.Pen() #Stylo permettant de dessiner le bonhomme pendu
+p_s = t.Pen() #Stylo permettant de mettre les "_" 
+p_l = t.Pen() #Stylo permettant d'écrire les lettres au-dessus des "_"
+p_t_l = t.Pen() #Stylo permettant d'écrire les lettres fausses déjà testées
 	
 while replay == 1:
 
@@ -104,14 +119,14 @@ while replay == 1:
 	found = False
 	num_error = 0
 	shadow_word(selected_word)
-	pos1 = posbegin(selected_word)
-	p_t_l.setpos(-300,-250)
-	a = ""
+	pos1 = originpos(selected_word)
+	p_t_l.setpos(-90,-275)
+	a = ""   # Variable permettant d'indiquer la nature de l'erreur de l'input
 	while not found and num_error < 7:
 		letter = t.textinput("Input",a + "Lettre :")
 		a = ""
 		if letter == None:
-			num_error = 7
+			num_error = 7	#Si le joueur appuye sur cancel on le fait directement perdre sa partie
 		elif iscorrect(letter) and letter.lower() not in tested_letters:
 			letter = letter.lower()
 			tested_letters.add(letter)
@@ -126,10 +141,10 @@ while replay == 1:
 				num_error += 1
 				error(num_error)
 		elif letter in tested_letters:
-			a = "Lettre déjà entroduite!\n"
+			a = "Lettre déjà introduite!\n"
 		else:
 			a = "Input incorrect!\n"
-
+		#Si l'ensemble des lettres du mot se trouve dans l'ensemble des lettres testées le found passe à True
 		found = (letters - tested_letters) == set()
 
 	if found:
